6b32c6491e89fc7c0f0dc75e092c9d916895ad97
Ahmad Saffiullah <sadapukhtoon777@gmail.com>
2024-05-30 20:56:11 +0500

    navigation install

---
+++ src/screens/getstart/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function GetStart() {
+    return (
+        <View>
+            <Text>GetStart</Text>
+        </View>
+    )
+}
